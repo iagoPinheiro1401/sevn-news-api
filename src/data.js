9d6c8bcbfbefa92dbc2b371914063a70d84109d8
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2024-06-05 10:58:58 -0300

    added mock date initials

--- src/data.js
+++ src/data.js
@@ -1,1 +1,178 @@
-//mock data
+export const artigosPrincipais = [
+    {
+        id: 1,
+        categoria: "Economia",
+        titulo: "Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases, diz BC",
+        conteudo: [
+            "Banco Central libera consulta a dinheiro esquecido em bancos",
+            "O Banco Central informou nesta segunda-feira (14) que quem não tiver valores a receber nesta etapa das consultas ao novo site de consulta aos recursos 'esquecidos' nos bancos poderá eventualmente ter nas próximas fases do sistema.",
+            "Muitos clientes que acessam o sistema têm recebido a mensagem para retornar a consulta em 2 de maio (veja na imagem mais abaixo).",
+            "\"Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases\", informou o BC ao ser questionado pelo g1.",
+            "Isso acontece porque, ao todo, os bancos têm R$ 8 bilhões a devolver aos clientes. Mas, nesta primeira fase, foram abertas consultas referentes à metade, R$ 4 bilhões. ",
+            "Banco Central libera consulta de dinheiro esquecido em bancos: saiba como fazer",
+            "Em 2 de maio, as consultas a uma nova fase serão abertas. O BC não informou, no entanto, se todos os R$ 4 bilhões restantes serão liberados para consultas nesta segunda fase. Em janeiro, no entanto, o BC afirmou que todos os recursos seriam liberados 'ao longo de 2022'.",
+            "Página do BC informa que cidadão sem valores a receber atualmente poderá fazer nova consulta a partir de maio."
+        ],
+        dataPostagem: "2024-06-05",
+        horarioPostagem: "14:30",
+        autor: "João Silva"
+    },
+    {
+        id: 2,
+        titulo: "Datafolha: Após ensino remoto, 76% precisam de reforço na alfabetização.",
+        categoria: "Educação",
+        conteudo: [
+            "\"É muita dispersão. A imagem trava, o cachorro começa a latir, passa o carro do gás tocando música na rua\"",
+            "O percentual é ainda mais alto entre crianças em fase de alfabetização, com 76% delas com necessidade de algum apoio para complementar o aprendizado, segundo pais e responsáveis ouvidos pela pesquisa.",
+            "De acordo com o levantamento, dois em cada dez estudantes correm o risco de abandonar a escola. Os principais motivos para a desistência são a perda de interesse nos estudos (29%) e a dificuldade em acompanhar as atividades (29%). Para 15% dos alunos, há também o medo de não serem acolhidos diante das dificuldades que tiveram no aprendizado durante o ensino remoto."
+        ],
+        dataPostagem: "2024-06-05",
+        horarioPostagem: "16:30",
+        autor: "Iago Pinheiro"
+    },
+    {
+        id: 3,
+        titulo: "Lotomania: com prêmio de R$ 5 milhões, veja os números sorteados hoje",
+        categoria: "Diversidades",
+        conteudo: [
+            "No concurso 3083 de sexta, duas apostas (Paraná e São Paulo) acertaram 15 números e levaram R$ 595.123,79. Os números sorteados foram: 02-04-05-07-08-09-10-11-12-15-18-19-20-21-23.",
+            "O próximo concurso é neste sábado e deve pagar R$ 1,7 milhão. Na região, Uma aposta de Araras feita pela internet fez 14 acertos e levou R$ 1.828,32. Outra de São Carlos levou R$ 914,16.",
+            "Uma aposta simples de Araras fez 4 números e levou R$ 8.894,55. A aposta foi feita na Lotérica Jardim Candida."
+        ],
+        dataPostagem: "2024-06-01",
+        horarioPostagem: "11:30",
+        autor: "Iago Pinheiro"
+    }
+];
+
+export const artigosSecundarios = [
+    {
+        id: 1,
+        categoria: "Economia",
+        titulo: "Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases, diz BC",
+        conteudo: [
+            "Banco Central libera consulta a dinheiro esquecido em bancos",
+            "O Banco Central informou nesta segunda-feira (14) que quem não tiver valores a receber nesta etapa das consultas ao novo site de consulta aos recursos 'esquecidos' nos bancos poderá eventualmente ter nas próximas fases do sistema.",
+            "Muitos clientes que acessam o sistema têm recebido a mensagem para retornar a consulta em 2 de maio (veja na imagem mais abaixo).",
+            "\"Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases\", informou o BC ao ser questionado pelo g1.",
+            "Isso acontece porque, ao todo, os bancos têm R$ 8 bilhões a devolver aos clientes. Mas, nesta primeira fase, foram abertas consultas referentes à metade, R$ 4 bilhões. ",
+            "Banco Central libera consulta de dinheiro esquecido em bancos: saiba como fazer",
+            "Em 2 de maio, as consultas a uma nova fase serão abertas. O BC não informou, no entanto, se todos os R$ 4 bilhões restantes serão liberados para consultas nesta segunda fase. Em janeiro, no entanto, o BC afirmou que todos os recursos seriam liberados 'ao longo de 2022'.",
+            "Página do BC informa que cidadão sem valores a receber atualmente poderá fazer nova consulta a partir de maio."
+        ],
+        dataPostagem: "2024-06-05",
+        horarioPostagem: "14:30",
+        autor: "João Silva"
+    },
+    {
+        id: 2,
+        categoria: "Economia",
+        titulo: "Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases, diz BC",
+        conteudo: [
+            "Banco Central libera consulta a dinheiro esquecido em bancos",
+            "O Banco Central informou nesta segunda-feira (14) que quem não tiver valores a receber nesta etapa das consultas ao novo site de consulta aos recursos 'esquecidos' nos bancos poderá eventualmente ter nas próximas fases do sistema.",
+            "Muitos clientes que acessam o sistema têm recebido a mensagem para retornar a consulta em 2 de maio (veja na imagem mais abaixo).",
+            "\"Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases\", informou o BC ao ser questionado pelo g1.",
+            "Isso acontece porque, ao todo, os bancos têm R$ 8 bilhões a devolver aos clientes. Mas, nesta primeira fase, foram abertas consultas referentes à metade, R$ 4 bilhões. ",
+            "Banco Central libera consulta de dinheiro esquecido em bancos: saiba como fazer",
+            "Em 2 de maio, as consultas a uma nova fase serão abertas. O BC não informou, no entanto, se todos os R$ 4 bilhões restantes serão liberados para consultas nesta segunda fase. Em janeiro, no entanto, o BC afirmou que todos os recursos seriam liberados 'ao longo de 2022'.",
+            "Página do BC informa que cidadão sem valores a receber atualmente poderá fazer nova consulta a partir de maio."
+        ],
+        dataPostagem: "2024-06-05",
+        horarioPostagem: "14:30",
+        autor: "João Silva"
+    },
+    {
+        id: 3,
+        categoria: "Economia",
+        titulo: "Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases, diz BC",
+        conteudo: [
+            "Banco Central libera consulta a dinheiro esquecido em bancos",
+            "O Banco Central informou nesta segunda-feira (14) que quem não tiver valores a receber nesta etapa das consultas ao novo site de consulta aos recursos 'esquecidos' nos bancos poderá eventualmente ter nas próximas fases do sistema.",
+            "Muitos clientes que acessam o sistema têm recebido a mensagem para retornar a consulta em 2 de maio (veja na imagem mais abaixo).",
+            "\"Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases\", informou o BC ao ser questionado pelo g1.",
+            "Isso acontece porque, ao todo, os bancos têm R$ 8 bilhões a devolver aos clientes. Mas, nesta primeira fase, foram abertas consultas referentes à metade, R$ 4 bilhões. ",
+            "Banco Central libera consulta de dinheiro esquecido em bancos: saiba como fazer",
+            "Em 2 de maio, as consultas a uma nova fase serão abertas. O BC não informou, no entanto, se todos os R$ 4 bilhões restantes serão liberados para consultas nesta segunda fase. Em janeiro, no entanto, o BC afirmou que todos os recursos seriam liberados 'ao longo de 2022'.",
+            "Página do BC informa que cidadão sem valores a receber atualmente poderá fazer nova consulta a partir de maio."
+        ],
+        dataPostagem: "2024-06-05",
+        horarioPostagem: "14:30",
+        autor: "João Silva"
+    },
+    {
+        id: 4,
+        categoria: "Economia",
+        titulo: "Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases, diz BC",
+        conteudo: [
+            "Banco Central libera consulta a dinheiro esquecido em bancos",
+            "O Banco Central informou nesta segunda-feira (14) que quem não tiver valores a receber nesta etapa das consultas ao novo site de consulta aos recursos 'esquecidos' nos bancos poderá eventualmente ter nas próximas fases do sistema.",
+            "Muitos clientes que acessam o sistema têm recebido a mensagem para retornar a consulta em 2 de maio (veja na imagem mais abaixo).",
+            "\"Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases\", informou o BC ao ser questionado pelo g1.",
+            "Isso acontece porque, ao todo, os bancos têm R$ 8 bilhões a devolver aos clientes. Mas, nesta primeira fase, foram abertas consultas referentes à metade, R$ 4 bilhões. ",
+            "Banco Central libera consulta de dinheiro esquecido em bancos: saiba como fazer",
+            "Em 2 de maio, as consultas a uma nova fase serão abertas. O BC não informou, no entanto, se todos os R$ 4 bilhões restantes serão liberados para consultas nesta segunda fase. Em janeiro, no entanto, o BC afirmou que todos os recursos seriam liberados 'ao longo de 2022'.",
+            "Página do BC informa que cidadão sem valores a receber atualmente poderá fazer nova consulta a partir de maio."
+        ],
+        dataPostagem: "2024-06-05",
+        horarioPostagem: "14:30",
+        autor: "João Silva"
+    },
+    {
+        id: 5,
+        titulo: "Datafolha: Após ensino remoto, 76% precisam de reforço na alfabetização.",
+        categoria: "Educação",
+        conteudo: [
+            "\"É muita dispersão. A imagem trava, o cachorro começa a latir, passa o carro do gás tocando música na rua\"",
+            "O percentual é ainda mais alto entre crianças em fase de alfabetização, com 76% delas com necessidade de algum apoio para complementar o aprendizado, segundo pais e responsáveis ouvidos pela pesquisa.",
+            "De acordo com o levantamento, dois em cada dez estudantes correm o risco de abandonar a escola. Os principais motivos para a desistência são a perda de interesse nos estudos (29%) e a dificuldade em acompanhar as atividades (29%). Para 15% dos alunos, há também o medo de não serem acolhidos diante das dificuldades que tiveram no aprendizado durante o ensino remoto."
+        ],
+        dataPostagem: "2024-06-05",
+        horarioPostagem: "16:30",
+        autor: "Iago Pinheiro"
+    },
+    {
+        id: 6,
+        titulo: "Datafolha: Após ensino remoto, 76% precisam de reforço na alfabetização.",
+        categoria: "Educação",
+        conteudo: [
+            "\"É muita dispersão. A imagem trava, o cachorro começa a latir, passa o carro do gás tocando música na rua\"",
+            "O percentual é ainda mais alto entre crianças em fase de alfabetização, com 76% delas com necessidade de algum apoio para complementar o aprendizado, segundo pais e responsáveis ouvidos pela pesquisa.",
+            "De acordo com o levantamento, dois em cada dez estudantes correm o risco de abandonar a escola. Os principais motivos para a desistência são a perda de interesse nos estudos (29%) e a dificuldade em acompanhar as atividades (29%). Para 15% dos alunos, há também o medo de não serem acolhidos diante das dificuldades que tiveram no aprendizado durante o ensino remoto."
+        ],
+        dataPostagem: "2024-06-05",
+        horarioPostagem: "16:30",
+        autor: "Iago Pinheiro"
+    },
+    {
+        id: 7,
+        titulo: "Lotomania: com prêmio de R$ 5 milhões, veja os números sorteados hoje",
+        categoria: "Diversidades",
+        conteudo: [
+            "No concurso 3083 de sexta, duas apostas (Paraná e São Paulo) acertaram 15 números e levaram R$ 595.123,79. Os números sorteados foram: 02-04-05-07-08-09-10-11-12-15-18-19-20-21-23.",
+            "O próximo concurso é neste sábado e deve pagar R$ 1,7 milhão. Na região, Uma aposta de Araras feita pela internet fez 14 acertos e levou R$ 1.828,32. Outra de São Carlos levou R$ 914,16.",
+            "Uma aposta simples de Araras fez 4 números e levou R$ 8.894,55. A aposta foi feita na Lotérica Jardim Candida."
+        ],
+        dataPostagem: "2024-06-01",
+        horarioPostagem: "11:30",
+        autor: "Iago Pinheiro"
+    },
+    {
+        id: 8,
+        titulo: "Lotomania: com prêmio de R$ 5 milhões, veja os números sorteados hoje",
+        categoria: "Diversidades",
+        conteudo: [
+            "No concurso 3083 de sexta, duas apostas (Paraná e São Paulo) acertaram 15 números e levaram R$ 595.123,79. Os números sorteados foram: 02-04-05-07-08-09-10-11-12-15-18-19-20-21-23.",
+            "O próximo concurso é neste sábado e deve pagar R$ 1,7 milhão. Na região, Uma aposta de Araras feita pela internet fez 14 acertos e levou R$ 1.828,32. Outra de São Carlos levou R$ 914,16.",
+            "Uma aposta simples de Araras fez 4 números e levou R$ 8.894,55. A aposta foi feita na Lotérica Jardim Candida."
+        ],
+        dataPostagem: "2024-06-01",
+        horarioPostagem: "11:30",
+        autor: "Iago Pinheiro"
+    }
+];
+
+export const noticiasDetalhes = [
+    ...artigosPrincipais,
+    ...artigosSecundarios
+];
