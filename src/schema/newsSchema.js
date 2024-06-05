f1c12da386702f60d35620386431d378736b7841
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2024-06-05 11:00:56 -0300

    added article validation with joi

--- src/schema/newsSchema.js
+++ src/schema/newsSchema.js
@@ -1,1 +1,11 @@
-//Define the data structure and validate the data against that structure using the Joi library.
+import Joi from 'joi';
+
+export const artigoSchema = Joi.object({
+    id: Joi.number().integer().required(),
+    titulo: Joi.string().required(),
+    categoria: Joi.string().required(),
+    conteudo: Joi.array().items(Joi.string()).required(),
+    dataPostagem: Joi.string().required(),
+    horarioPostagem: Joi.string().required(),
+    autor: Joi.string().required()
+});
